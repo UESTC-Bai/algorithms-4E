5aa8c4f0c7b3ed34e87f2e0a3ea744e29178753f
Yu Bai <baiyu940822@gmail.com>
2018-08-27 17:01:01 +0800

    Add files via upload

---
+++ Chapter1/1.5/ch1x5x13.java
@@ -0,0 +1,69 @@
+public class ch1x5x13 {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public ch1x5x13(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            sz[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        int root = p;
+        while (root != id[root]) {
+            root = id[root];
+        }
+        while (id[p] != root) {
+            int temp = p;
+            p = id[p];
+            id[temp] = root;
+        }
+        return root;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) {
+            return;
+        }
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        ch1x5x13 uf = new ch1x5x13(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) {
+                continue;
+            }
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}
---
+++ Chapter1/1.5/ch1x5x14.java
@@ -0,0 +1,62 @@
+public class ch1x5x14 {
+    private int[] id;
+    private int[] height;
+    private int count;
+
+    public ch1x5x14(int N) {
+        count = N;
+        id = new int[N];
+        height = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            height[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) {
+            return;
+        }
+        if (height[i] < height[j]) {
+            id[i] = j;
+        } else {
+            id[j] = i;
+            if (height[i] == height[j]) {
+                height[i]++;
+            }
+        }
+        count--;
+    }
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        ch1x5x14 uf = new ch1x5x14(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) {
+                continue;
+            }
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}
